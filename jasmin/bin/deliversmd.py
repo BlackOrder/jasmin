823f4213b231a82a63c5b2fe3b40d6db4065dbc9
BlackOrder <4302157+BlackOrder@users.noreply.github.com>
2024-01-16 17:42:38 +0000

    Add AMQP disconnection error handling in DeliverSmDaemon. Update logging

--- jasmin/bin/deliversmd.py
+++ jasmin/bin/deliversmd.py
@@ -3,7 +3,9 @@
 import os
 import signal
 import sys
-import syslog
+import traceback
+import logging
+import ntpath
 
 from lockfile import FileLock, LockTimeout, AlreadyLocked
 from twisted.internet import reactor, defer
@@ -18,8 +20,10 @@ from jasmin.routing.throwers import deliverSmThrower
 from jasmin.config import ROOT_PATH
 from jasmin.bin import BaseDaemon
 
+
 CONFIG_PATH = os.getenv('CONFIG_PATH', '%s/etc/jasmin/' % ROOT_PATH)
 
+LOG_CATEGORY = "jasmin-deliversm-daemon"
 
 class Options(usage.Options):
     optParameters = [
@@ -34,15 +38,31 @@ class Options(usage.Options):
 
 
 class DeliverSmDaemon(BaseDaemon):
+    def __init__(self, opt):
+        super(DeliverSmDaemon, self).__init__(opt)
+
+        self.log = logging.getLogger(LOG_CATEGORY)
+        self.log.setLevel(logging.INFO)
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(process)d %(message)s', '%Y-%m-%d %H:%M:%S')
+        handler.setFormatter(formatter)
+        self.log.addHandler(handler)
+        self.log.propagate = False
+
+    @defer.inlineCallbacks
     def startAMQPBrokerService(self):
         """Start AMQP Broker"""
 
         AMQPServiceConfigInstance = AmqpConfig(self.options['config'])
+        
+        # This is a separate process: do not log to same log_file as Jasmin Daemon
+        AMQPServiceConfigInstance.log_file = '%s/deliversmd-%s' % ntpath.split(AMQPServiceConfigInstance.log_file)
+
         self.components['amqp-broker-factory'] = AmqpFactory(AMQPServiceConfigInstance)
         self.components['amqp-broker-factory'].preConnect()
 
         # Add service
-        self.components['amqp-broker-client'] = reactor.connectTCP(
+        self.components['amqp-broker-client'] = yield reactor.connectTCP(
             AMQPServiceConfigInstance.host,
             AMQPServiceConfigInstance.port,
             self.components['amqp-broker-factory'])
@@ -65,22 +85,28 @@ class DeliverSmDaemon(BaseDaemon):
             SMPPServerPBClientConfigInstance.password,
             retry=True)
 
+    @defer.inlineCallbacks
     def stopSMPPServerPBClient(self):
         """Stop SMPPServerPB client"""
 
         if self.components['smpps-pb-client'].isConnected:
-            return self.components['smpps-pb-client'].disconnect()
+            yield self.components['smpps-pb-client'].disconnect()
 
     def startdeliverSmThrowerService(self):
         """Start deliverSmThrower"""
 
         deliverThrowerConfigInstance = deliverSmThrowerConfig(self.options['config'])
+        
+        # This is a separate process: do not log to same log_file as Jasmin Daemon
+        deliverThrowerConfigInstance.log_file = '%s/deliversmd-%s' % ntpath.split(deliverThrowerConfigInstance.log_file)
+        
         self.components['deliversm-thrower'] = deliverSmThrower(deliverThrowerConfigInstance)
         self.components['deliversm-thrower'].addSmpps(self.components['smpps-pb-client'])
 
         # AMQP Broker is used to listen to deliver_sm queue
         return self.components['deliversm-thrower'].addAmqpBroker(self.components['amqp-broker-factory'])
 
+    @defer.inlineCallbacks
     def stopdeliverSmThrowerService(self):
         """Stop deliverSmThrower"""
         return self.components['deliversm-thrower'].stopService()
@@ -88,61 +114,91 @@ class DeliverSmDaemon(BaseDaemon):
     @defer.inlineCallbacks
     def start(self):
         """Start Deliver-Sm Daemon"""
-        syslog.syslog(syslog.LOG_INFO, "Starting Deliver-Sm Daemon ...")
+        self.log.info("Starting Deliver-Sm Daemon ...")
 
         ########################################################
         # Start AMQP Broker
         try:
-            self.startAMQPBrokerService()
+            yield self.startAMQPBrokerService()
             yield self.components['amqp-broker-factory'].getChannelReadyDeferred()
-        except Exception as e:
-            syslog.syslog(syslog.LOG_ERR, "  Cannot start AMQP Broker: %s" % e)
-        else:
-            syslog.syslog(syslog.LOG_INFO, "  AMQP Broker Started.")
+            self.log.info("  AMQP Broker Started.")
 
-        ########################################################
-        try:
+            ########################################################
+            # register services stop on channelDown
+            self.components['amqp-broker-factory'].getChannelDownDeferred().addCallback(self.AMQPDownHandler)
+
+            ########################################################
             # Start SMPPServerPB Client
-            yield self.startSMPPServerPBClient()
-        except Exception as e:
-            syslog.syslog(syslog.LOG_ERR, "  Cannot start SMPPServerPBClient: %s" % e)
-        else:
-            syslog.syslog(syslog.LOG_INFO, "  SMPPServerPBClient Started.")
+            try:
+                yield self.startSMPPServerPBClient()
+                self.log.info("  SMPPServerPBClient Started.")
+                
+                ########################################################
+                # Start deliverSmThrower
+                try:
+                    yield self.startdeliverSmThrowerService()
+                    self.log.info("  deliverSmThrower Started.")
 
-        ########################################################
-        try:
-            # Start deliverSmThrower
-            yield self.startdeliverSmThrowerService()
+                except Exception as e:
+                    self.log.error("  Cannot start deliverSmThrower: %s\n%s" % (e, traceback.format_exc()))
+            except Exception as e:
+                self.log.error("  Cannot start SMPPServerPBClient: %s\n%s" % (e, traceback.format_exc()))
         except Exception as e:
-            syslog.syslog(syslog.LOG_ERR, "  Cannot start deliverSmThrower: %s" % e)
+            self.log.error("  Cannot start AMQP Broker: %s\n%s" % (e, traceback.format_exc()))
+
+    def AMQPDownHandler(self, ignore=None):
+        """Pause Deliver-Smd services"""
+        self.log.error("AMQP Broker disconnected, pausing Deliver-Smd services ...")
+        if self.components['amqp-broker-factory'].connected is True:
+            return self.refreshServices()
+
+        if self.components['amqp-broker-factory'].connectionRetry:
+            self.components['amqp-broker-factory'].getChannelReadyDeferred().addCallback(self.refreshServices).addErrback(self.stop)
         else:
-            syslog.syslog(syslog.LOG_INFO, "  deliverSmThrower Started.")
+            self.stop()
 
     @defer.inlineCallbacks
-    def stop(self):
+    def refreshServices(self, ignore=None):
+        """Refresh Deliver-Smd services"""
+        self.log.info("Refreshing Deliver-Smd services ...")
+
+        self.components['amqp-broker-factory'].getChannelDownDeferred().addCallback(self.AMQPDownHandler)
+
+        ########################################################
+        try:
+            # Refresh deliverSmThrower
+            if 'deliversm-thrower' in self.components:
+                yield self.stopdeliverSmThrowerService()
+            yield self.startdeliverSmThrowerService()
+        except Exception as e:
+            self.log.error("  Cannot refresh deliverSmThrower: %s\n%s" % (e, traceback.format_exc()))
+        else:
+            self.log.info("  deliverSmThrower Refreshed.")
+
+    @defer.inlineCallbacks
+    def stop(self, ignore=None):
         """Stop Deliver-Sm daemon"""
-        syslog.syslog(syslog.LOG_INFO, "Stopping Deliver-Sm Daemon ...")
+        self.log.info("Stopping Deliver-Sm Daemon ...")
 
         if 'smpps-pb-client' in self.components:
             yield self.stopSMPPServerPBClient()
-            syslog.syslog(syslog.LOG_INFO, "  SMPPServerPBClient stopped.")
+            self.log.info("  SMPPServerPBClient Started.")
 
         if 'deliversm-thrower' in self.components:
             yield self.stopdeliverSmThrowerService()
-            syslog.syslog(syslog.LOG_INFO, "  deliverSmThrower stopped.")
+            self.log.info("  deliverSmThrower stopped.")
 
         if 'amqp-broker-client' in self.components:
             yield self.stopAMQPBrokerService()
-            syslog.syslog(syslog.LOG_INFO, "  AMQP Broker disconnected.")
-
-        reactor.stop()
+            self.log.info("  AMQP Broker disconnected.")
+        
+        yield reactor.stop()
 
     def sighandler_stop(self, signum, frame):
         """Handle stop signal cleanly"""
-        syslog.syslog(syslog.LOG_INFO, "Received signal to stop Deliver-Sm Daemon")
+        self.log.info("Received signal to stop Deliver-Sm Daemon")
 
         return self.stop()
-
 
 if __name__ == '__main__':
     lock = None
